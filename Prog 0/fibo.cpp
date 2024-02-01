88729e1fc9dcf0d673db740390436a552322c893
aiden-perkins <perkinsaidenm@gmail.com>
2024-02-01 11:07:59 -0800

    prog 0

---
+++ Prog 0/fibo.cpp
@@ -0,0 +1,23 @@
+// Author: Aiden Perkins
+// CECS 325-02 Prog 0
+// Due 02/01/2023
+#include <iostream>
+using namespace std;
+
+// Fibo function
+int fibo(int n)
+{
+	if (n == 1 || n == 0)
+		return 1;
+	else
+		return fibo(n - 1) + fibo(n - 2); // recursion
+}
+
+int main()
+{
+	for (int i = 0; i <= 20; i ++)
+		cout << i << ":" << fibo(i) << endl;
+	
+	return 0;
+}
+

cfbed1218d16cacebe4a7fb0e4c033e0cb247a88
aiden-perkins <perkinsaidenm@gmail.com>
2024-03-10 22:09:21 -0700

    add header

--- Prog 3/mysort.cpp
+++ Prog 3/mysort.cpp
@@ -1,3 +1,11 @@
+// Aiden Perkins
+// CECS 325-02
+// Prog 3 – BubbleSort
+// Due 3-12-2024
+//
+// I certify that this program is my own original work. I did not copy any part of this program from
+// any other source. I further certify that I typed each and every line of code in this program.
+
 #include <iostream>
 #include <fstream>
 #include <string>

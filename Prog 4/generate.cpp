966dff941fb70db0d069638dfd8adc7f4912ed7c
aiden-perkins <perkinsaidenm@gmail.com>
2024-03-15 13:29:11 -0700

    finish prog 4
    I will probably have to change a few things later on when we learn more during lectures but for now this seems complete.

---
+++ Prog 4/generate.cpp
@@ -0,0 +1,28 @@
+// Aiden Perkins
+// CECS 325-02
+// Prog 4 – Sorting Contest using pThreads
+// Due 3-26-2024
+//
+// I certify that this program is my own original work. I did not copy any part of this program from
+// any other source. I further certify that I typed each and every line of code in this program.
+
+#include <iostream>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        cout << "Did not provide 3 arguments." << endl;
+        return 0;
+    }
+    srand(time(nullptr));
+    ofstream numbersFile("numbers.dat");
+    int min = stoi(argv[2]);
+    int mod = stoi(argv[3]) - min + 1;
+    for (int i = 0; i < stoi(argv[1]); i++) {
+        numbersFile << rand() % mod + min << '\n';
+    }
+    return 0;
+}
